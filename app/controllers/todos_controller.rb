3bf2642e668b81c6e35f6735ded41bf28203fa4f
Mr-DareDevil <arnabsen1729@gmail.com>
2021-01-19 20:43:43 +0530

    Added create todo endpoint

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,15 @@ class TodosController < ApplicationController
     id = params[:id]
     render plain: Todo.find(id).to_pleasant_string
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    render plain: "Hey your new todo was created with id #{new_todo.id}"
+  end
 end
