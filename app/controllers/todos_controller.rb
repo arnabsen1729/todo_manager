87a29d8c4381022eb5903cac46c4661bb07c1134
Mr-DareDevil <arnabsen1729@gmail.com>
2021-01-19 20:27:36 +0530

    Added routes for /todos and /todos/:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,10 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    render plain: Todo.find(id).to_pleasant_string
   end
 end
