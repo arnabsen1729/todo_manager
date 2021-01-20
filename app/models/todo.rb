a5f3f13202a3be485e74ec0ef58dda57a5789a34
Mr-DareDevil <arnabsen1729@gmail.com>
2021-01-20 16:41:24 +0530

    Upd: Added ERB and rendering the todos dynamically

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,32 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
   end
+
+  def self.add_task(todo)
+    create!(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days], completed: false)
+  end
+
+  def self.mark_as_complete!(todo_id)
+    update(todo_id, completed: true)
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    where(completed: true)
+  end
 end
